1fced80bc69e1151c851fc32b44066384d01ea91
WayfareTraining <WayfareTraining@wayfare.local>
2022-06-23 13:28:49 +0300

    fixed some bugs

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -14,15 +14,9 @@ Array.from(elements).forEach(function (element) {
 let ids = [];
 
 for (let i = 0; i <= elements.length; i++) {
-
-    ids[i] = elements[i].id;
-    console.log(ids);
-
-}
-
-for (let j = 0; j <= ids.length; j++) {
-    if (ids[j] !== '') {
-        ids.splice(j, 1);
+    if (elements[i].id !== '') {
+        ids[i] = elements[i].id;
+        console.log(ids);
     }
+
 }
-console.log(ids);
