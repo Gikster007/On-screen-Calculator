1380f8a41151e3cf348ac392a98abc3183da50ec
WayfareTraining <WayfareTraining@wayfare.local>
2022-06-23 13:25:19 +0300

    added more functionality

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -7,3 +7,22 @@ let myFunction = function (event) {
 Array.from(elements).forEach(function (element) {
     element.addEventListener('click', myFunction);
 });
+
+
+//If the ID of the clicked button is === "multiply" OR ""
+
+let ids = [];
+
+for (let i = 0; i <= elements.length; i++) {
+
+    ids[i] = elements[i].id;
+    console.log(ids);
+
+}
+
+for (let j = 0; j <= ids.length; j++) {
+    if (ids[j] !== '') {
+        ids.splice(j, 1);
+    }
+}
+console.log(ids);
